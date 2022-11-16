47097950221992d225969c7268da656cf454b929
Markus Wüstenberg <markus@maragu.dk>
2022-11-16 11:14:24 +0100

    Add heroicons

--- html/pages.go
+++ html/pages.go
@@ -2,13 +2,14 @@ package html
 
 import (
 	g "github.com/maragudk/gomponents"
+	"github.com/maragudk/gomponents-heroicons/v2/solid"
 	. "github.com/maragudk/gomponents/html"
 )
 
 func HomePage() g.Node {
 	return Page(PageProps{Title: "Service", Description: "This is a service."},
 
-		H1(g.Text(`Service`)),
+		H1(Class("inline-flex items-center"), solid.Sparkles(Class("h-12 w-12 mr-2")), g.Text(`Service`)),
 		P(g.Raw(`Hi! 🤓`)),
 	)
 }
