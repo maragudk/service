879f38a15657d84b62cef0fbcebd01bbf32b7468
Markus Wüstenberg <markus@maragu.dk>
2022-11-23 22:08:26 +0100

    Add Charter as default serif font
    Also prepare for selecting different mono/sans fonts.

--- html/pages.go
+++ html/pages.go
@@ -9,7 +9,12 @@ import (
 func HomePage() g.Node {
 	return Page(PageProps{Title: "Service", Description: "This is a service."},
 
-		H1(Class("inline-flex items-center"), solid.Sparkles(Class("h-12 w-12 mr-2")), g.Text(`Service`)),
-		P(g.Raw(`Hi! 🤓`)),
+		Div(Class("prose-headings:font-serif"),
+			H1(Class("inline-flex items-center"), solid.Sparkles(Class("h-12 w-12 mr-2")), g.Text(`Service`)),
+
+			P(Class("lead"), g.Raw(`Hi! 🤓 This is a service template in Go.`)),
+
+			P(g.Raw(`<a href="https://github.com/maragudk/service">Check out the source code on Github</a>. It’s nice.`)),
+		),
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'mono': [...defaultTheme.fontFamily.mono],
+        'sans': [...defaultTheme.fontFamily.sans],
+        'serif': ['Charter', ...defaultTheme.fontFamily.serif],
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
