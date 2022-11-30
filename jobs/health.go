f8014ac23eeaf4c16506543358748554aacf058c
Markus Wüstenberg <markus@maragu.dk>
2022-11-30 11:45:01 +0100

    Don't print log on job success (#24)
    Doesn't really add much value that isn't handled by metrics.
    
    Also delegate the registered job logging to the runner itself.

--- jobs/health.go
+++ jobs/health.go
@@ -2,12 +2,11 @@ package jobs
 
 import (
 	"context"
-	"log"
 
 	"github.com/maragudk/service/model"
 )
 
-func health(r registry, log *log.Logger) {
+func Health(r registry) {
 	r.Register("health", func(ctx context.Context, m model.Map) error {
 		return nil
 	})
--- jobs/register.go
+++ jobs/register.go
@@ -1,17 +1,5 @@
 package jobs
 
-import (
-	"sort"
-)
-
 func (r *Runner) registerJobs() {
-	health(r, r.log)
-
-	var names []string
-	for k := range r.jobs {
-		names = append(names, k)
-	}
-	sort.Strings(names)
-
-	r.log.Println("Registered jobs:", names)
+	Health(r)
 }
